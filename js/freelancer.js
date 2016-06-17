c9a8990194febc2f220cc64f854b3114a1e2fb2d
das01 <vivek.das@dskgig.in>
2016-06-17 17:08:01 +0530

    upload-17/06

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,60 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+
+
+
+
+// Vivek Changes//
+
+
+
+// Get the modal
+var modal = document.getElementById('contentFull');
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('contentImage');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// Get the modal
+var modal = document.getElementById('contentFull');
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('contentImage1');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
